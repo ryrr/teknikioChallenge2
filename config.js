65887ad3382e5c54d46739e823b506271607abfb
riprr <rpr288@nyu.edu>
2020-12-07 18:16:57 -0500

    security patch

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 module.exports = {
     db:{
-        host: "localhost",
-        port:'3306',
-        user: "root",
-        password: "3LongPenguins",
-        database:'carshare'
+       
     },
     app:{port:3000},
 
