09f4a4096dce53fb127c1ceed2c8cddad9d780dc
riprr <rpr288@nyu.edu>
2020-12-07 21:14:03 -0500

    hid session secret

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
        
     },
     app:{port:3000},
+    sessionSecret:{}
 
 }
--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 app.use(cookieParser());
-app.use(session({secret: "Shh, its a secret!", resave: true, saveUninitialized: true}));
+app.use(session({secret: config.sessionSecret.secret, resave: true, saveUninitialized: true}));
 
 app.use("/",routes)
 
